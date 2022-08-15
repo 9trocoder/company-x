c6c89b068e8ef143462c7113575b7b1e5f657409
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-08-15 20:50:22 +0300

    added already have an account

--- src/pages/Onboarding/Signup.js
+++ src/pages/Onboarding/Signup.js
@@ -9,15 +9,28 @@ function Signup() {
     <>
       <div className="signup-container">
         <div className="signup-container-txt">
-          <h1>Sign Up</h1>
-          <p>Required fields have an asterisk:*</p>
+          <h1 className="signup-txtheader">Sign Up</h1>
+          <p className="signup-cnt-text">Required fields have an asterisk:*</p>
         </div>
 
         <form action="" className="signup-form">
           <input className="firstboard-input" type="text" value={email} />
-          <input className="firstboard-input" type="text" placeholder="Your name*" />
-          <input className="firstboard-input" type="text" placeholder="Your password*" />
-          <button className="firstboard-button">Sign Up</button>
+          <input
+            className="firstboard-input"
+            type="text"
+            placeholder="Your full name*"
+          />
+          <input
+            className="firstboard-input"
+            type="password"
+            placeholder="Your password*"
+          />
+          <div className="thedown">
+            <button className="firstboard-button">Sign Up</button>
+
+            <p className="alreadybtn">Already have an account ? <span>Log In</span></p>
+          </div>
+          
         </form>
       </div>
     </>
