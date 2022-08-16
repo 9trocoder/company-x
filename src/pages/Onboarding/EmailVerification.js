bf48b2e46a7819bb6649843e0b29d34e15274239
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-08-16 18:31:18 +0300

    styled email verification page

--- src/pages/Onboarding/EmailVerification.js
+++ src/pages/Onboarding/EmailVerification.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import "./EmailVerification.css";
 
 function EmailVerification() {
   const { state } = useLocation();
@@ -7,8 +8,8 @@ function EmailVerification() {
   return (
     <>
       <div className="verifyemailbody">
-        <h1>Let's go</h1>
-        <p>We just emailed you @ {email}, Kindly verify your email address</p>
+        <h1 className="firstboard-header">Let's go</h1>
+        <p>We just emailed you at {email}, Kindly verify your email address</p>
         <button className="firstboard-button">Resend Email Verification</button>
       </div>
     </>
